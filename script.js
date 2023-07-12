0b1dcf37aabad2969e3de16b30467e6b15760151
JoseChinchilla <j.l.chinchillaspn@gmail.com>
2023-07-12 16:33:37 +0200

    Second Commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function(task) {
-          $('#todo-list').append('<div class="border-bottom px-2 py-2 active"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><span class="mx-2 ' + (task.completed ? 'text-complete' : '') +'">' + task.content + '</span><button class="btn btn-sm btn-outline-danger delete" data-id="' + task.id + '">Delete</button></div>');
+          $('#todo-list').append('<div class="' + (task.display === false ? 'd-none ' : '') + 'border-bottom px-2 py-2 active"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><span class="mx-2 ' + (task.completed ? 'text-complete' : '') +'">' + task.content + '</span><button class="btn btn-sm btn-outline-danger delete" data-id="' + task.id + '">Delete</button></div>');
         });
         $('#items-left').text(response.tasks.length + ' left');
       },
@@ -43,6 +43,13 @@ $(document).ready(function() {
     createTask();
   });
 
+  $('#create-task').keypress(function(e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      createTask();
+    }
+  });
+
   let deleteTask = function(id) {
     $.ajax({
       type: 'DELETE',
@@ -63,8 +70,8 @@ $(document).ready(function() {
   $(document).on('click', '#clear-completed', function(e) {
     e.preventDefault();
     $('#todo-list').children().each(function() {
-      if (this.checked) {
-        deleteTask($(this).data('id'));
+      if ($(this).find('.mark-complete').prop('checked')) {
+        deleteTask($(this).find('.mark-complete').data('id'));
       }
     });
   });
@@ -107,6 +114,71 @@ $(document).ready(function() {
     } else {
       markTaskActive($(this).data('id'));
     }
+  });
+
+  let unableDisplay = function(id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=231',
+      dataType: 'json',
+      data: JSON.stringify({ display: true }),
+      contentType: 'application/json',
+      success: function(response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  let disableDisplay = function(id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=231',
+      dataType: 'json',
+      data: JSON.stringify({display: false}),
+      contentType: 'application/json',
+      success: function(response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+  
+  $(document).on('click', '.selector', function(e) {
+    e.preventDefault();
+    switch ($(this).attr('id')) {
+      case 'show-all':
+        $('#todo-list').children().each(function() {
+          unableDisplay($(this).data('id'));
+        });
+        break;
+      case 'show-active':
+        $('#todo-list').children().each(function() {
+          if ($(this).find('.mark-complete').prop('checked')) {
+            disableDisplay($(this).data('id'));
+          } else {
+            unableDisplay($(this).data('id'));
+          }
+        });
+        break;
+      case 'show-completed':
+        $('#todo-list').children().each(function() {
+          if (!$(this).find('.mark-complete').prop('checked')) {
+            disableDisplay($(this).data('id'));
+          } else {
+            unableDisplay($(this).data('id'));
+          }
+        });
+        break;
+    }
+    $('.selectors').children().each(function() {
+      $(this).removeClass('border border-dark rounded');
+    });
+    $(this).addClass('border border-dark rounded');
   });
 
   getAndDisplayAllTasks();
